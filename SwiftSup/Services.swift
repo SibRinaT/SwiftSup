5db7275ee0f87e77ab9e75326ab644cf4ae09386
Ainur <sibrinat616@gmail.com>
2024-12-17 10:33:43 +0300

    service

---
+++ SwiftSup/Services.swift
@@ -0,0 +1,20 @@
+//
+//  Services.swift
+//  SwiftSup
+//
+//  Created by Ainur on 17.12.2024.
+//
+
+import Supabase
+import SwiftUI
+
+final class SupabaseManager {
+    static let shared = SupabaseManager()
+    
+    private let supabaseURL = URL(string: "https://phdelqmezlrdoabsavlt.supabase.com")!
+    private let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoZGVscW1lemxyZG9hYnNhdmx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQxNzAyODMsImV4cCI6MjA0OTc0NjI4M30.0A4jYJVq-dEfqvvrY-fToRCAbM_trOvUafpJZlqi9fk"
+    
+    lazy var client: SupabaseClient = {
+        SupabaseClient(supabaseURL: supabaseURL, supabaseKey: supabaseKey)
+    }()
+}
