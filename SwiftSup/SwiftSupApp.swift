2399b2d46dade4e867403ece6deef76f18edee19
Ainur <sibrinat616@gmail.com>
2024-12-17 10:09:11 +0300

    first commit

---
+++ SwiftSup/SwiftSupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSupApp.swift
+//  SwiftSup
+//
+//  Created by Ainur on 17.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftSupTests/SwiftSupTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSupTests.swift
+//  SwiftSupTests
+//
+//  Created by Ainur on 17.12.2024.
+//
+
+import Testing
+@testable import SwiftSup
+
+struct SwiftSupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
